009bdac9b78bd0d94f3ca5979e3bf1cb5a09fbbb
Taima Rahman <taimarh@gmail.com>
2025-12-19 21:21:41 +0600

    Initial commit

---
+++ src/main/java/com/project/SpringSecurityEx/SpringSecurityExApplication.java
@@ -0,0 +1,13 @@
+package com.project.SpringSecurityEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityExApplication.class, args);
+	}
+
+}
