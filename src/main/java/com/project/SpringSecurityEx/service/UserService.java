e62c84825ded82ca278a5edcf56816b6cefcd604
Taima Rahman <taimarh@gmail.com>
2025-12-20 15:55:26 +0600

    add BCryptPasswordEncoder

--- src/main/java/com/project/SpringSecurityEx/service/UserService.java
+++ src/main/java/com/project/SpringSecurityEx/service/UserService.java
@@ -4,6 +4,7 @@ import com.project.SpringSecurityEx.model.Users;
 import com.project.SpringSecurityEx.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,10 @@ public class UserService {
 
     @Autowired
     private UserRepo userRepo;
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(12);
 
     public Users register(Users user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepo.save(user);
     }
 }
